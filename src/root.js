157f8abdfc744a30c9af615b16d668c33b95f9b1
spohngellert-o <spohngellert.o@husky.neu.edu>
2017-09-24 12:48:00 -0400

    First commit!

---
+++ src/root.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import AppContainer from './containers/AppContainer';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<p>hello world!</p>, document.getElementById('todoapp'));
